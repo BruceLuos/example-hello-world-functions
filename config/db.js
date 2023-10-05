d117c21b9f4fa7c6df55a63d89c05049d9fff455
BruceLuo <295448382@qq.com>
2023-10-05 17:31:06 +0800

    getTemplate

---
+++ config/db.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** MONGODB **/
+const mongodbUrl = process.env.MONGODB_URL
+const mongodbDbName = 'luo-cli';
+
+module.exports = {
+  mongodbUrl,
+  mongodbDbName,
+};
---
+++ netlify/functions/getTemplate.js
@@ -0,0 +1,26 @@
+// hello there!
+//
+// I'm a serverless function that you can deploy as part of your site.
+// I'll get deployed to AWS Lambda, but you don't need to know that.
+// You can develop and deploy serverless functions right here as part
+// of your site. Netlify Functions will handle the rest for you.
+const mongo = require("../../utils/mongo");
+
+exports.handler = async (event) => {
+  try {
+    // const subject = event.queryStringParameters.name || 'World'
+    // const { ctx } = this;
+    const data = await mongo().query("project");
+    console.log("data", data);
+    // ctx.body = data;
+    return {
+      statusCode: 200,
+      body: data,
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: "获取数据失败",
+    };
+  }
+};
